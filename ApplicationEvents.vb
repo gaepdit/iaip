e4ebb4c0ef49697e9e81769b74daea03e8ae1ce4
Doug Waldron <none@none>
2013-07-29 15:19:37 -0400

    Add EQATEC analytics monitor

--- ApplicationEvents.vb
+++ ApplicationEvents.vb
@@ -1,4 +1,6 @@
-﻿Namespace My
+﻿Imports EQATEC.Analytics.Monitor
+
+Namespace My
 
     ' The following events are available for MyApplication:
     ' 
@@ -8,10 +10,26 @@
     ' StartupNextInstance: Raised when launching a single-instance application and the application is already active. 
     ' NetworkAvailabilityChanged: Raised when the network connection is connected or disconnected.
     Partial Friend Class MyApplication
+        Friend monitor As IAnalyticsMonitor = AnalyticsMonitorFactory.CreateMonitor("094F22FFB35C42E9A5D65279634F5028")
 
-        'Private Sub MyApplication_Startup(ByVal sender As Object, ByVal e As Microsoft.VisualBasic.ApplicationServices.StartupEventArgs) Handles Me.Startup
+        Private Sub MyApplication_Startup(ByVal sender As Object, ByVal e As Microsoft.VisualBasic.ApplicationServices.StartupEventArgs) _
+            Handles Me.Startup
 
-        'End Sub
+            monitor.Start()
+
+            '' Just for fun: sample code for a new way of handling login form
+            '' http://visualstudiomagazine.com/articles/2008/08/01/customize-your-application-startup.aspx
+            'Dim frm = New LoginForm
+            'Dim result = frm.ShowDialog
+            'If result <> DialogResult.OK Then
+            '    e.Cancel = True
+            '    Me.HideSplashScreen()
+            'End If
+        End Sub
+
+        Private Sub MyApplication_Shutdown(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Shutdown
+            monitor.Stop()
+        End Sub
 
     End Class
 
