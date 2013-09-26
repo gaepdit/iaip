dd71ffd068afb14868aae99a28744c5db5ebc67b
Doug Waldron <none@none>
2013-09-26 13:25:45 -0400

    Improve deployment script

--- IAIP-Deploy/Deploy-IAIP.bat
+++ IAIP-Deploy/Deploy-IAIP.bat
@@ -1,10 +1,13 @@
 @echo off
 cls
 
+ECHO : 
 ECHO : IAIP deployment script
 ECHO : ----------------------
 ECHO : 
+
 :ask
+
 ECHO : Deploy IAIP (Y/N)?
 
 SET /P "ANSWER=: "
@@ -19,64 +22,86 @@ ECHO :
 ECHO : Deploying...
 ECHO : 
 
-IF NOT EXIST S:\ISMP\DMU\Execs\appIAIP GOTO :NoSDrive
+REM --- Copy files that CopyGATV.bat will look for
 
-ECHO : Copying files to "S:\ISMP\DMU\Execs\appIAIP"...
-ECHO :
+IF EXIST "S:\ISMP\DMU\Execs\appIAIP" (
 
-COPY /y "..\*.*" "S:\ISMP\DMU\Execs\appIAIP"
-ECHO :
+  ECHO : Copying files to "S:\ISMP\DMU\Execs\appIAIP"...
+  ECHO :
 
-GOTO :installer
+  COPY /y "..\*.*" "S:\ISMP\DMU\Execs\appIAIP"
+  
+  ECHO :
 
-:NoSDrive
+  IF NOT EXIST "S:\ISMP\DMU\Execs\appIAIP\docs" (
+    MKDIR  "S:\ISMP\DMU\Execs\appIAIP\docs"
+  )
 
-ECHO : "S:\ISMP\DMU\Execs\appIAIP" is not available...
-ECHO :
+  IF EXIST "S:\ISMP\DMU\Execs\appIAIP" (
+    COPY /y "..\docs\*.*" "S:\ISMP\DMU\Execs\appIAIP\docs"
+  )
 
-GOTO :installer
+) ELSE (
 
-:installer
+  ECHO : "S:\ISMP\DMU\Execs\appIAIP" is not available...
+  ECHO :
 
-IF NOT EXIST "S:\ISMP\DMU\APB IAIP\Installer" GOTO :NoInstaller
+)
 
-ECHO : Copying installer to "S:\ISMP\DMU\APB IAIP\Installer"...
-ECHO :
+REM --- Copy installer to S Drive
 
-COPY /y "..\IAIP-Installer\*.zip" "S:\ISMP\DMU\APB IAIP\Installer"
-ECHO :
+IF EXIST "S:\ISMP\DMU\APB IAIP\Installer" (
 
-GOTO :ftp
+  ECHO : Copying installer to "S:\ISMP\DMU\APB IAIP\Installer"...
+  ECHO :
 
-:NoInstaller
+  COPY /y "..\IAIP-Installer\*.zip" "S:\ISMP\DMU\APB IAIP\Installer"
+  
+  ECHO :
 
-ECHO : "S:\ISMP\DMU\APB IAIP\Installer" is not available...
-ECHO :
+) ELSE (
 
-GOTO :ftp
+  ECHO : "S:\ISMP\DMU\APB IAIP\Installer" is not available...
+  ECHO :
 
-:ftp
+)
 
-IF NOT EXIST "..\IAIP-Installer" GOTO :NoFTP
-IF NOT EXIST "..\IAIP-Updater" GOTO :NoFTP
+REM --- Upload files to FTP site
 
+IF EXIST "..\IAIP-Installer" (
 
-ECHO : Uploading files to FTP site...
-ECHO : 
-ftp -v -i -s:ftp-script.txt
-ECHO : 
+  IF EXIST "..\IAIP-Updater" (
 
-GOTO :done
+    ECHO : Uploading files to FTP site...
+    ECHO : 
+    
+    FTP -v -i -s:ftp-script.txt
+	
+    ECHO : 
 
-:NoFTP
+  ) ELSE (
 
-ECHO : Files to upload to FTP site don't exist...
-ECHO :
+    ECHO : Files to upload to FTP site don't exist...
+    ECHO :
 
-:done
+  )
+
+) ELSE (
+
+  ECHO : Files to upload to FTP site don't exist...
+  ECHO :
+
+)
 
 ECHO : Script has finished.
 ECHO :
+ECHO : Remaining tasks:
+ECHO : 
+ECHO : * Move the update files on the server from /AirPermit/IAIP/new
+ECHO :   to /AirPermit/IAIP (and backup the existing files there)
+EHCO : 
+ECHO : * Change the published version number in the Platform AFS tools
+ECHO :
 PAUSE
 
-GOTO :eof
+:eof
