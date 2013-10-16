290ee40c36a8b4b6cba163bcfdea7ae3bc59e575
Doug Waldron <none@none>
2013-10-16 11:50:48 -0400

    Change form icon on BaseForm when in testing environment

--- Universal Forms/BaseForm.vb
+++ Universal Forms/BaseForm.vb
@@ -1,3 +1,9 @@
 ﻿Public Class BaseForm
 
+#If Not Debug Then
+    Private Sub BaseForm_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        If TestingEnvironment Then Me.Icon = My.Resources.TestingIcon
+    End Sub
+#End If
+
 End Class
