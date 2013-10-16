16db963799a391a8e3c7e38ee33ab026769af313
Doug Waldron <none@none>
2013-10-16 15:20:00 -0400

    Classes for generic Document, Permit Document, and Enforcement Document

---
+++ _Classes/Document.vb
@@ -0,0 +1,121 @@
+﻿Imports System.IO
+
+Public Class Document
+
+    Private _documentId As Integer
+    Public Property DocumentId() As Integer
+        Get
+            Return _documentId
+        End Get
+        Set(ByVal value As Integer)
+            _documentId = value
+        End Set
+    End Property
+
+    Private _binaryFileId As Integer
+    Public Property BinaryFileId() As Integer
+        Get
+            Return _binaryFileId
+        End Get
+        Set(ByVal value As Integer)
+            _binaryFileId = value
+        End Set
+    End Property
+
+    Private _fileName As String
+    Public Property FileName() As String
+        Get
+            Return _fileName
+        End Get
+        Set(ByVal value As String)
+            _fileName = value
+        End Set
+    End Property
+
+    Public ReadOnly Property FileExtension() As String
+        Get
+            Return Path.GetExtension(_fileName)
+        End Get
+    End Property
+
+    Private _fileSize As Integer
+    Public Property FileSize() As Integer
+        Get
+            Return _fileSize
+        End Get
+        Set(ByVal value As Integer)
+            _fileSize = value
+        End Set
+    End Property
+
+    Private _documentType As String
+    Public Property DocumentType() As String
+        Get
+            Return _documentType
+        End Get
+        Set(ByVal value As String)
+            _documentType = value
+        End Set
+    End Property
+
+    Private _documentTypeId As Integer
+    Public Property DocumentTypeId() As Integer
+        Get
+            Return _documentTypeId
+        End Get
+        Set(ByVal value As Integer)
+            _documentTypeId = value
+        End Set
+    End Property
+
+    Private _comment As String
+    Public Property Comment() As String
+        Get
+            Return _comment
+        End Get
+        Set(ByVal value As String)
+            _comment = value
+        End Set
+    End Property
+
+    Private _uploadDate As Date?
+    Public Property UploadDate() As Date?
+        Get
+            Return _uploadDate
+        End Get
+        Set(ByVal value As Date?)
+            _uploadDate = value
+        End Set
+    End Property
+
+End Class
+
+Public Class PermitDocument
+    Inherits Document
+
+    Private _applicationNumber As String
+    Public Property ApplicationNumber() As String
+        Get
+            Return _applicationNumber
+        End Get
+        Set(ByVal value As String)
+            _applicationNumber = value
+        End Set
+    End Property
+
+End Class
+
+Public Class EnforcementDocument
+    Inherits Document
+
+    Private _enforcementNumber As String
+    Public Property EnforcementNumber() As String
+        Get
+            Return _enforcementNumber
+        End Get
+        Set(ByVal value As String)
+            _enforcementNumber = value
+        End Set
+    End Property
+
+End Class
