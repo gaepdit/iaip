c1abd1b28ab9ec71bec15ab76487e3867ee10f1d
Doug Waldron <none@none>
2013-10-16 15:18:59 -0400

    Classes for Address and Location

---
+++ _Classes/Address.vb
@@ -0,0 +1,109 @@
+﻿Namespace Apb
+
+    Public Class Address
+        ' Address is simply all the elements of a postal address
+
+        Public Property Street() As String
+            Get
+                Return _street
+            End Get
+            Set(ByVal value As String)
+                _street = value
+            End Set
+        End Property
+        Private _street As String
+        Public Property Street2() As String
+            Get
+                Return _street2
+            End Get
+            Set(ByVal value As String)
+                _street2 = value
+            End Set
+        End Property
+        Private _street2 As String
+        Public Property City() As String
+            Get
+                Return _city
+            End Get
+            Set(ByVal value As String)
+                _city = value
+            End Set
+        End Property
+        Private _city As String
+        Public Property State() As String
+            Get
+                Return _state
+            End Get
+            Set(ByVal value As String)
+                _state = value
+            End Set
+        End Property
+        Private _state As String
+        Public Property PostalCode() As String
+            Get
+                Return _postalCode
+            End Get
+            Set(ByVal value As String)
+                _postalCode = value
+            End Set
+        End Property
+        Private _postalCode As String
+        Public Property Country() As String
+            Get
+                Return _country
+            End Get
+            Set(ByVal value As String)
+                _country = value
+            End Set
+        End Property
+        Private _country As String
+
+        Public Overrides Function ToString() As String
+            Dim aLines As String() = {Me.Street, Me.Street2, Convert.ToString((Convert.ToString(Me.City & Convert.ToString(", ")) & Me.State) + " ") & Me.PostalCode}
+            Dim fAddress As String = String.Join(Constants.vbNewLine, Array.FindAll(aLines, Function(s) Not String.IsNullOrEmpty(s)))
+            Return fAddress
+        End Function
+    End Class
+
+    Public Class Location
+        ' Location is a postal address + lat/lon and county
+
+        Public Property Address() As Address
+            Get
+                Return _address
+            End Get
+            Set(ByVal value As Address)
+                _address = value
+            End Set
+        End Property
+        Private _address As Address
+        Public Property Latitude() As Nullable(Of Decimal)
+            Get
+                Return _latitude
+            End Get
+            Set(ByVal value As Nullable(Of Decimal))
+                _latitude = value
+            End Set
+        End Property
+        Private _latitude As Nullable(Of Decimal)
+        Public Property Longitude() As Nullable(Of Decimal)
+            Get
+                Return _longitude
+            End Get
+            Set(ByVal value As Nullable(Of Decimal))
+                _longitude = value
+            End Set
+        End Property
+        Private _longitude As Nullable(Of Decimal)
+        Public Property County() As String
+            Get
+                Return _county
+            End Get
+            Set(ByVal value As String)
+                _county = value
+            End Set
+        End Property
+        Private _county As String
+    End Class
+
+End Namespace
