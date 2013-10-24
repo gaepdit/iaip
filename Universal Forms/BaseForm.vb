10ebb75ac79803c2eec114955517d02fb0f71600
Doug Waldron <none@none>
2013-10-24 15:03:36 -0400

    BaseForm - Try to prevent Visual Studio crashes in Release mode

--- Universal Forms/BaseForm.vb
+++ Universal Forms/BaseForm.vb
@@ -1,9 +1,13 @@
 ﻿Public Class BaseForm
 
 #If Not Debug Then
+
     Private Sub BaseForm_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-        If TestingEnvironment Then Me.Icon = My.Resources.TestingIcon
+        If Not Me.DesignMode Then
+            If TestingEnvironment Then Me.Icon = My.Resources.TestingIcon
+        End If
     End Sub
+
 #End If
 
 End Class
