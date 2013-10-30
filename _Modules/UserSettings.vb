aff17d7d5995fb8b28c3ec4483b1d0c11ec43c4d
Doug Waldron <none@none>
2013-10-30 09:23:04 -0400

    Split SettingHelper class from UserSettings module

--- _Modules/UserSettings.vb
+++ _Modules/UserSettings.vb
@@ -1,7 +1,4 @@
-﻿Imports System.Collections.Generic
-Imports System.Runtime.Serialization
-
-Module UserSettings
+﻿Module UserSettings
 
     ' Define user settings here
     Friend Enum UserSetting As Byte
@@ -9,7 +6,7 @@ Module UserSettings
         PermitUploadLocation
         FileDownloadLocation
         PrefillLoginId
-        NavigationFormLocation
+        FormDimensions
     End Enum
 
     ' Define default value for above user settings here
@@ -21,10 +18,8 @@ Module UserSettings
                 UserSetting.FileDownloadLocation
                 Return Environment.GetFolderPath(Environment.SpecialFolder.Personal)
 
-            Case UserSetting.NavigationFormLocation
-                Return "0,0"
-
-            Case UserSetting.PrefillLoginId
+            Case UserSetting.PrefillLoginId, _
+                UserSetting.FormDimensions
                 Return ""
 
             Case Else
@@ -35,8 +30,8 @@ Module UserSettings
 
     ' Public function for retrieving a setting
     Friend Function GetUserSetting(ByVal whichSetting As UserSetting) As String
-        If UserSettingsHelper.KeySettingsDictionary.ContainsKey(whichSetting.ToString) Then
-            Return UserSettingsHelper.KeySettingsDictionary(whichSetting.ToString)
+        If SettingsHelper.KeySettingsDictionary.ContainsKey(whichSetting.ToString) Then
+            Return SettingsHelper.KeySettingsDictionary(whichSetting.ToString)
         Else
             Return DefaultSetting(whichSetting)
         End If
@@ -44,71 +39,12 @@ Module UserSettings
 
     ' Public function for saving a setting
     Friend Sub SaveUserSetting(ByVal whichSetting As UserSetting, ByVal value As String)
-        UserSettingsHelper.KeySettingsDictionary(whichSetting.ToString) = value
+        SettingsHelper.KeySettingsDictionary(whichSetting.ToString) = value
     End Sub
 
     ' Public function for deleting a setting (resetting to default)
     Friend Sub ResetUserSetting(ByVal whichSetting As UserSetting)
-        UserSettingsHelper.KeySettingsDictionary.Remove(whichSetting.ToString)
+        SettingsHelper.KeySettingsDictionary.Remove(whichSetting.ToString)
     End Sub
-
-    ' Adapted from http://stackoverflow.com/a/11801369/212978
-    Public Class UserSettingsHelper
-        Private Shared _keySettingsDictionary As Dictionary(Of String, String)
-        Private Shared _initLock As Object = New Object()
-
-        Public Shared ReadOnly Property KeySettingsDictionary() As Dictionary(Of String, String)
-            Get
-                If (_keySettingsDictionary Is Nothing) Then
-                    InitializeDictionary()
-                End If
-                Return _keySettingsDictionary
-            End Get
-        End Property
-
-        Shared Sub New()
-            AddHandler My.Settings.SettingsLoaded, AddressOf HandleSettingsLoad
-            AddHandler My.Settings.SettingsSaving, AddressOf HandleSettingsSaving
-        End Sub
-
-        Private Shared Sub InitializeDictionary()
-            ' Load dictionary from User Setting.
-            SyncLock _initLock
-                If (_keySettingsDictionary Is Nothing) Then
-                    If (String.IsNullOrEmpty(My.Settings.SerializedUserSettingsDictionary)) Then
-                        _keySettingsDictionary = New Dictionary(Of String, String)()
-                    Else
-                        Dim ser As New System.Runtime.Serialization.Json.DataContractJsonSerializer(GetType(Dictionary(Of String, String)))
-                        Using memStream As New System.IO.MemoryStream()
-                            Using writer As New System.IO.StreamWriter(memStream)
-                                writer.Write(My.Settings.SerializedUserSettingsDictionary)
-                                writer.Flush()
-                                memStream.Position = 0
-                                _keySettingsDictionary = CType(ser.ReadObject(memStream), Dictionary(Of String, String))
-                            End Using
-                        End Using
-                    End If
-                End If
-            End SyncLock
-        End Sub
-
-        Private Shared Sub HandleSettingsLoad(ByVal sender As Object, ByVal e As EventArgs)
-            If (_keySettingsDictionary Is Nothing) Then
-                InitializeDictionary()
-            End If
-        End Sub
-
-        Private Shared Sub HandleSettingsSaving(ByVal sender As Object, ByVal e As EventArgs)
-            ' Ensure User Setting value is updated before save.
-            Dim ser As New System.Runtime.Serialization.Json.DataContractJsonSerializer(GetType(Dictionary(Of String, String)))
-            Using memStream As New System.IO.MemoryStream()
-                ser.WriteObject(memStream, _keySettingsDictionary)
-                memStream.Position = 0
-                Using reader As New System.IO.StreamReader(memStream)
-                    My.Settings.SerializedUserSettingsDictionary = reader.ReadToEnd()
-                End Using
-            End Using
-        End Sub
-    End Class
 
 End Module
