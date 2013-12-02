3bf79cb8452cc2967b9d6bdcc4bf4d5a92f1c4bf
Doug Waldron <none@none>
2013-12-02 15:14:24 -0500

    Separate enforcement and permit documents from main Documents class

---
+++ _Data Classes/SSCP.Document.vb
@@ -0,0 +1,25 @@
+﻿Imports System.IO
+Imports Oracle.DataAccess.Types
+
+Namespace Apb
+
+    Namespace SSCP
+
+        Public Class EnforcementDocument
+            Inherits Document
+
+            Private _enforcementNumber As String
+            Public Property EnforcementNumber() As String
+                Get
+                    Return _enforcementNumber
+                End Get
+                Set(ByVal value As String)
+                    _enforcementNumber = value
+                End Set
+            End Property
+
+        End Class
+
+    End Namespace
+
+End Namespace
