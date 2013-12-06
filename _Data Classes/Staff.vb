4fee57f13f7a552b398a9f465a4cbb60be1e041d
Doug Waldron <none@none>
2013-12-06 15:41:49 -0500

    Separate Person Class from Staff Class

--- _Data Classes/Staff.vb
+++ _Data Classes/Staff.vb
@@ -1,4 +1,5 @@
 ﻿Public Class Staff
+    Inherits Person
 
     Public Property StaffId() As Integer
         Get
@@ -19,61 +20,5 @@
         End Set
     End Property
     Private _activeStatus As Integer
-
-    Public Property FirstName() As String
-        Get
-            Return _firstName
-        End Get
-        Set(ByVal value As String)
-            _firstName = value
-        End Set
-    End Property
-    Private _firstName As String
-
-    Public Property LastName() As String
-        Get
-            Return _lastName
-        End Get
-        Set(ByVal value As String)
-            _lastName = value
-        End Set
-    End Property
-    Private _lastName As String
-
-    Public Property Email() As String
-        Get
-            Return _email
-        End Get
-        Set(ByVal value As String)
-            _email = value
-        End Set
-    End Property
-    Private _email As String
-
-    Public Property Phone() As String
-        Get
-            Return _phone
-        End Get
-        Set(ByVal value As String)
-            _phone = value
-        End Set
-    End Property
-    Private _phone As String
-
-    Public Overrides Function ToString() As String
-        Return Convert.ToString(LastName & ", " & FirstName)
-    End Function
-
-    Public ReadOnly Property FullName() As String
-        Get
-            Return Convert.ToString(FirstName & " " & LastName)
-        End Get
-    End Property
-
-    Public ReadOnly Property AlphaName() As String
-        Get
-            Return Convert.ToString(LastName & ", " & FirstName)
-        End Get
-    End Property
 
 End Class
