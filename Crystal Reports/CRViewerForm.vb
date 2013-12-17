f724ca3d0886423765b69bc1c2ad9941c0571e0a
Doug Waldron <none@none>
2013-12-17 16:30:08 -0500

    Add CRViewerForm to act as generic Crystal Report Viewer control/form
    --HG--
    branch : feature-crystal-reports

---
+++ Crystal Reports/CRViewerForm.vb
@@ -0,0 +1,30 @@
+﻿Option Strict On
+
+Imports CrystalDecisions.CrystalReports.Engine
+Imports CrystalDecisions.Shared
+
+Public Class CRViewerForm
+
+    Private _crSource As ReportDocument
+    Public Property CRDocumentSource() As ReportDocument
+        Get
+            Return _crSource
+        End Get
+        Set(ByVal value As ReportDocument)
+            _crSource = value
+        End Set
+    End Property
+
+    Private Sub CrystalReportViewerForm_FormClosed(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosedEventArgs) Handles Me.FormClosed
+        CRDocumentSource.Close()
+    End Sub
+
+    Private Sub CrystalReportViewerForm_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        ConfigureCrystalReports()
+    End Sub
+
+    Private Sub ConfigureCrystalReports()
+        If CRDocumentSource IsNot Nothing Then CRViewerControl.ReportSource = CRDocumentSource
+    End Sub
+
+End Class
