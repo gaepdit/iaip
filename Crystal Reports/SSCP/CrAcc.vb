20354661be598cda3c3ad322ee8713b5be860e28
Doug Waldron <none@none>
2013-12-20 13:32:55 -0500

    Add new ACC Memo Crystal Report and CrAcc class (because current version can't handle deep objects)
    --HG--
    branch : feature-crystal-reports

---
+++ Crystal Reports/SSCP/CrAcc.vb
@@ -0,0 +1,54 @@
+﻿Namespace CR.Data
+
+    Public Class CrAcc
+        Inherits Apb.SSCP.Acc
+
+        Public Sub New(ByVal acc As Apb.SSCP.Acc)
+            Me.AccReportingYear = acc.AccReportingYear
+            Me.AllTitleVConditionsListed = acc.AllTitleVConditionsListed
+            Me.Comments = acc.Comments
+            Me.CorrectFormsUsed = acc.CorrectFormsUsed
+            Me.CorrectlyFilledOut = acc.CorrectlyFilledOut
+            Me.DateAcknowledgmentLetterSent = acc.DateAcknowledgmentLetterSent
+            Me.DateComplete = acc.DateComplete
+            Me.DatePostmarked = acc.DatePostmarked
+            Me.DateReceived = acc.DateReceived
+            Me.Deleted = acc.Deleted
+            Me.DeviationsReported = acc.DeviationsReported
+            Me.EnforcementNeeded = acc.EnforcementNeeded
+            Me.Facility = acc.Facility
+            Me.SignedByResponsibleOfficial = acc.SignedByResponsibleOfficial
+            Me.StaffResponsible = acc.StaffResponsible
+            Me.SubmittalNumber = acc.SubmittalNumber
+            Me.TrackingNumber = acc.TrackingNumber
+            Me.UnreportedDeviationsReported = acc.UnreportedDeviationsReported
+        End Sub
+
+
+        Public ReadOnly Property FacilityName() As String
+            Get
+                Return Me.Facility.Name
+            End Get
+        End Property
+
+        Public ReadOnly Property FacilityAirsNumber() As String
+            Get
+                Return Me.Facility.AirsNumber
+            End Get
+        End Property
+
+        Public ReadOnly Property StaffName() As String
+            Get
+                Return Me.StaffResponsible.FullName
+            End Get
+        End Property
+
+        Public ReadOnly Property FacilityCity() As String
+            Get
+                Return Me.Facility.FacilityLocation.Address.City
+            End Get
+        End Property
+
+    End Class
+
+End Namespace
