c1ebbe4746f0a03fe148e955733e6d1e668f376c
Doug Waldron <none@none>
2013-12-23 08:48:23 -0500

    Move NormalizeDate from FacilityUtility module to App module

--- _Modules/App.vb
+++ _Modules/App.vb
@@ -266,4 +266,17 @@ Module App
 
 #End Region
 
+#Region "Date functions"
+
+    Public Function NormalizeDate(ByVal d As Date?) As Date?
+        ' Converts a date to Nothing if date is equal to #7/4/1776#
+
+        If d.Equals(CType(Nothing, Date)) Then Return d
+        If Not IsDate(d) Then Return Nothing
+        If d.Equals(New Date(1776, 7, 4)) Then Return Nothing
+        Return d
+    End Function
+
+#End Region
+
 End Module
--- _Modules/FacilityUtility.vb
+++ _Modules/FacilityUtility.vb
@@ -2,14 +2,6 @@
 
     Module FacilityUtility
 
-        Public Function NormalizeDate(ByVal d As Date?) As Date?
-            ' Converts a date to Nothing if date is equal to #7/4/1776#
-
-            If d.Equals(CType(Nothing, Date)) Then Return d
-            If Not IsDate(d) Then Return Nothing
-            If d.Equals(New Date(1776, 7, 4)) Then Return Nothing
-            Return d
-        End Function
 
     End Module
 
