9b3ceee9437ddb6e1914fdc9d49c286b4672d294
Doug Waldron <none@none>
2014-01-07 09:48:59 -0500

    Added FirstRun setting and procedure

--- IAIP/_Modules/StartupShutdown.vb
+++ IAIP/_Modules/StartupShutdown.vb
@@ -17,6 +17,13 @@
             My.Settings.CallUpgrade = False
         End If
 
+        ' First Run
+        ' Should only run the first time a new installation is run
+        ' The False setting should be migrated by above Upgrade()
+        If My.Settings.FirstRun Then
+            FirstRun()
+        End If
+
         ' EQATEC analytics monitor
         MonitorInit()
 
@@ -34,6 +41,18 @@
         ' EQATEC analytics monitor
         MonitorStop()
 
+    End Sub
+
+    ''' <summary>
+    ''' Should only execute the first time a new installation is run
+    ''' </summary>
+    ''' <remarks>
+    ''' Flips FirstRun setting to false when run
+    ''' Can be used for setup requirements
+    ''' </remarks>
+    Friend Sub FirstRun()
+        If Not My.Settings.FirstRun Then Exit Sub
+        My.Settings.FirstRun = False
     End Sub
 
 End Module
