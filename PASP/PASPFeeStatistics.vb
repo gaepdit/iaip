946a5d76cd72d11754d4ead0d7b63f259f35ff25
Doug Waldron <none@none>
2014-01-15 14:18:23 -0500

    Move DisplayReport from MRFunctions to PASPFeeReports and PASPFeeStatistics

--- PASP/PASPFeeStatistics.vb
+++ PASP/PASPFeeStatistics.vb
@@ -10706,5 +10706,42 @@ Public Class PASPFeeStatistics
         End Try
     End Sub
 
+#Region "Crystal Reports displayer"
+
+    Sub DisplayReport(ByVal crReport As Object, ByVal TabText As String)
+        Try
+            crReport.DisplayGroupTree = True
+            crReport.DisplayToolbar = True
+            crReport.showrefreshbutton = False
+            crReport.visible = True
+            crReport.DisplayGroupTree = True
+
+            Dim I As Integer
+            Do While I < crReport.Controls.Count
+                If TypeOf (crReport.Controls(I)) Is CrystalDecisions.Windows.Forms.PageView Then
+                    Dim J As Integer
+                    Do While J < crReport.Controls(I).Controls.Count
+                        If CType(crReport.Controls(I).Controls(J), System.Windows.Forms.TabControl).TabPages.Count > 0 Then
+                            'Change the tab text..
+                            CType(crReport.Controls(I).Controls(J), System.Windows.Forms.TabControl).TabPages.Item(0).Text = TabText
+                            Exit Do
+                        End If
+                    Loop
+                    Exit Do
+                Else
+                    crReport.Controls(I).Visible = False
+                End If
+            Loop
+        Catch ex As Exception
+            ErrorReport(ex, "MRFunctions.DisplayReport")
+        Finally
+            If Conn.State = ConnectionState.Open Then
+                'conn.close()
+            End If
+        End Try
+
+    End Sub
+
+#End Region
 
 End Class
--- _MFCode/MRFunctions.vb
+++ _MFCode/MRFunctions.vb
@@ -16,40 +16,6 @@ Module MRFunctions
     'Public mstAttachFile As String
     'Public Apath As String = System.Environment.CurrentDirectory
 
-    Sub DisplayReport(ByVal crReport As Object, ByVal TabText As String)
-        Try
-            crReport.DisplayGroupTree = True
-            crReport.DisplayToolbar = True
-            crReport.showrefreshbutton = False
-            crReport.visible = True
-            crReport.DisplayGroupTree = True
-
-            Dim I As Integer
-            Do While I < crReport.Controls.Count
-                If TypeOf (crReport.Controls(I)) Is CrystalDecisions.Windows.Forms.PageView Then
-                    Dim J As Integer
-                    Do While J < crReport.Controls(I).Controls.Count
-                        If CType(crReport.Controls(I).Controls(J), System.Windows.Forms.TabControl).TabPages.Count > 0 Then
-                            'Change the tab text..
-                            CType(crReport.Controls(I).Controls(J), System.Windows.Forms.TabControl).TabPages.Item(0).Text = TabText
-                            Exit Do
-                        End If
-                    Loop
-                    Exit Do
-                Else
-                    crReport.Controls(I).Visible = False
-                End If
-            Loop
-        Catch ex As Exception
-            ErrorReport(ex, "MRFunctions.DisplayReport")
-        Finally
-            If Conn.State = ConnectionState.Open Then
-                'conn.close()
-            End If
-        End Try
-
-    End Sub
-
 #Region "Obsolete code removed"
 
     'Public Class RandomPassword
