b97f61ca90dbfa6716af80ea390d5e69c9f1ac0d
Doug Waldron <none@none>
2014-04-04 09:40:03 -0400

    Bump version numbers for next release

--- IAIP/AssemblyInfo.vb
+++ IAIP/AssemblyInfo.vb
@@ -29,6 +29,6 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("3.0.0.0")> 
+<Assembly: AssemblyVersion("3.1.0.0")> 
 
-<Assembly: AssemblyFileVersionAttribute("3.0.0.0")> 
+<Assembly: AssemblyFileVersionAttribute("3.1.0.0")> 
