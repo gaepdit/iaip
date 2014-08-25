08d51c9ac752dcf6f474e035741d971f2fd4979a
Doug Waldron <douglas.waldron@dnr.state.ga.us>
2014-08-25 11:25:46 -0400

    Bump version numbers
    --HG--
    branch : stable

--- IAIP/AssemblyInfo.vb
+++ IAIP/AssemblyInfo.vb
@@ -29,6 +29,6 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("3.5.5.0")> 
+<Assembly: AssemblyVersion("3.5.6.0")> 
 
-<Assembly: AssemblyFileVersionAttribute("3.5.5.0")> 
+<Assembly: AssemblyFileVersionAttribute("3.5.6.0")> 
