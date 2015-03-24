5e8819205859dd49e3137f5b5ccd41ab8fb20686
Doug Waldron <douglas.waldron@dnr.state.ga.us>
2015-03-24 11:26:27 -0400

    EDT Errors data model
    --HG--
    branch : feat/edt-errors

---
+++ IAIP/_Data Classes/DMU.EdtError.vb
@@ -0,0 +1,25 @@
+﻿Namespace IAIP
+    Public Class EdtError
+
+        Public Property ID() As String
+            Get
+                Return _id
+            End Get
+            Set(ByVal value As String)
+                _id = value
+            End Set
+        End Property
+        Private _id As String
+
+        Public Property ErrorCode() As String
+            Get
+                Return _errorCode
+            End Get
+            Set(ByVal value As String)
+                _errorCode = value
+            End Set
+        End Property
+        Private _errorCode As String
+
+    End Class
+End Namespace
