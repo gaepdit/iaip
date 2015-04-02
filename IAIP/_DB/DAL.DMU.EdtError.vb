f5d2792f56c977f8548bb98a045ad5d578b1e473
Doug Waldron <douglas.waldron@dnr.state.ga.us>
2015-04-02 16:44:02 -0400

    Complete EDT error data model and DAL
    --HG--
    branch : feat/edt-errors

---
+++ IAIP/_DB/DAL.DMU.EdtError.vb
@@ -0,0 +1,223 @@
+﻿Imports Oracle.DataAccess.Client
+Imports Iaip.DMU
+
+Namespace DAL.DMU
+
+    Module EdtErrorModule
+
+#Region " Read "
+
+        ''' <summary>
+        ''' Returns information about an EDT error message (description, category, etc.)
+        ''' </summary>
+        ''' <param name="errorCode">The EDT Error Code to retrieve information about</param>
+        ''' <returns>An EdtErrorMessage object</returns>
+        Public Function GetErrorMessageDetail(ByVal errorCode As String) As EdtErrorMessage
+            Dim em As EdtErrorMessage = Nothing
+            Dim spName As String = "ICIS_EDT_GetErrorMessageDetail"
+            Dim parameter As OracleParameter = New OracleParameter("errorCode", errorCode)
+
+            Dim dt As DataTable = DB.SPGetDataTable(spName, parameter)
+
+            If dt IsNot Nothing AndAlso dt.Rows.Count = 1 Then
+                em = MakeEdtErrorMessageFrom(dt.Rows(0))
+            End If
+
+            Return em
+        End Function
+
+        Private Function MakeEdtErrorMessageFrom(ByVal row As DataRow) As EdtErrorMessage
+            If row Is Nothing Then Return Nothing
+
+            Dim em As New EdtErrorMessage
+            With em
+                .BusinessRuleCode = DB.GetNullable(Of String)(row("BUSINESSRULECODE"))
+                .BusinessRuleMessage = DB.GetNullable(Of String)(row("BUSINESSRULE"))
+                .DefaultUserID = DB.GetNullable(Of Integer)(row("DEFAULTUSER"))
+                .DefaultUserName = DB.GetNullable(Of String)(row("DefaultUserName"))
+                .ErrorCategory = DB.GetNullable(Of String)(row("CATEGORY"))
+                .ErrorCode = DB.GetNullable(Of String)(row("ERRORCODE"))
+                .ErrorMessage = DB.GetNullable(Of String)(row("ERRORMESSAGE"))
+            End With
+
+            Return em
+        End Function
+
+        ''' <summary>
+        ''' Returns summary information on all EDT errors reported organized by EDT Error Code. Includes counts of 
+        ''' all errors reported, open errors, and errors assigned to specified user.
+        ''' </summary>
+        ''' <param name="userID">The user ID for which to return error counts</param>
+        ''' <returns>A DataTable</returns>
+        Public Function GetErrorCounts(ByVal userID As Integer) As DataTable
+            Dim spName As String = "ICIS_EDT_GetErrorCounts"
+            Dim parameter As OracleParameter = New OracleParameter("userID", userID)
+
+            Return DB.SPGetDataTable(spName, parameter)
+        End Function
+
+        ''' <summary>
+        ''' Returns all EDT errors reported for a specific EDT Error Code
+        ''' </summary>
+        ''' <param name="errorCode">The Error Code for which to return errors</param>
+        ''' <returns>A DataTable</returns>
+        Public Function GetErrors(ByVal errorCode As String) As DataTable
+            Dim spName As String = "ICIS_EDT_GetErrors"
+            Dim parameter As OracleParameter = New OracleParameter("errorCode", errorCode)
+
+            Return DB.SPGetDataTable(spName, parameter)
+        End Function
+
+        ''' <summary>
+        ''' Returns details on a specific error reported by EDT
+        ''' </summary>
+        ''' <param name="errorID">The Error ID to retrieve</param>
+        ''' <returns>An EdtError Object</returns>
+        Public Function GetErrorDetail(ByVal errorID As String) As EdtError
+            Dim er As EdtError = Nothing
+            Dim spName As String = "ICIS_EDT_GetErrorDetail"
+            Dim parameter As OracleParameter = New OracleParameter("errorID", errorID)
+
+            Dim dt As DataTable = DB.SPGetDataTable(spName, parameter)
+
+            If dt IsNot Nothing AndAlso dt.Rows.Count = 1 Then
+                er = MakeEdtErrorDetailFrom(dt.Rows(0))
+            End If
+
+            Return er
+        End Function
+
+        Private Function MakeEdtErrorDetailFrom(ByVal row As DataRow) As EdtError
+            If row Is Nothing Then Return Nothing
+
+            Dim es As New EdtSubmission
+            With es
+                .EdtForeignKeyID = DB.GetNullable(Of String)(row("FOREIGNKEY"))
+                .EdtID = DB.GetNullable(Of Integer)(row("ERRORID"))
+                .EdtOperation = DB.GetNullable(Of String)(row("OPERATION"))
+                .EdtStatus = DB.GetNullable(Of String)(row("STATUS"))
+                .EdtSubmitDate = DB.GetNullable(Of Date)(row("SUBMITDATE"))
+                .EdtTableName = DB.GetNullable(Of String)(row("TABLENAME"))
+            End With
+
+            Dim em As New EdtErrorMessage
+            With em
+                .BusinessRuleCode = DB.GetNullable(Of String)(row("BUSINESSRULECODE"))
+                .BusinessRuleMessage = DB.GetNullable(Of String)(row("BUSINESSRULE"))
+                .DefaultUserID = DB.GetNullable(Of Integer)(row("DEFAULTUSER"))
+                .DefaultUserName = DB.GetNullable(Of String)(row("DefaultUserName"))
+                .ErrorCategory = DB.GetNullable(Of String)(row("CATEGORY"))
+                .ErrorCode = DB.GetNullable(Of String)(row("ERRORCODE"))
+                .ErrorMessage = DB.GetNullable(Of String)(row("ERRORMESSAGE"))
+            End With
+
+            Dim er As New EdtError
+            With er
+                .AssignedToUserID = DB.GetNullable(Of String)(row("ASSIGNEDTOUSER"))
+                .EdtErrorMessageDetail = DB.GetNullable(Of String)(row("STATUSDETAIL"))
+                .EdtSubmission = es
+                .ErrorID = DB.GetNullable(Of String)(row("ERRORID"))
+                .ErrorMessage = em
+                .Resolved = Convert.ToBoolean(DB.GetNullable(Of String)(row("RESOLVED")))
+                .ResolvedByUserID = DB.GetNullable(Of Integer)(row("RESOLVEDBYUSER"))
+                .ResolvedByUserName = DB.GetNullable(Of String)(row("ResolvedByUserName"))
+                .ResolvedDate = DB.GetNullable(Of Date)(row("RESOLVEDDATE"))
+            End With
+
+            Return er
+        End Function
+
+#End Region
+
+#Region " Write "
+
+        ''' <summary>
+        ''' Specify the default user for an EDT error message code
+        ''' </summary>
+        ''' <param name="errorCode">The EDT error code for which to specify the default user</param>
+        ''' <param name="defaultUserID">The User ID to set as the default for the error code</param>
+        ''' <returns>True if the action was successful; otherwise false</returns>
+        Public Function SetDefaultUser(ByVal errorCode As String, ByVal defaultUserID As Integer) As Boolean
+            Dim spName As String = "ICIS_EDT_SetDefaultUser"
+
+            Dim parameters As OracleParameter() = { _
+                New OracleParameter("errorCode", errorCode), _
+                New OracleParameter("defaultUserID", defaultUserID) _
+            }
+
+            Return DB.SPRunCommand(spName, parameters)
+        End Function
+
+        ''' <summary>
+        ''' Resolve or re-open an EDT error record
+        ''' </summary>
+        ''' <param name="resolved">True to set as resolved; false to set as open</param>
+        ''' <param name="errorID">The EDT error ID</param>
+        ''' <returns>True if the action was successful; otherwise false</returns>
+        Public Function SetResolvedStatus(ByVal resolved As Boolean, ByVal errorID As String) As Boolean
+            Dim errorIDs As String() = {errorID}
+            Return SetResolvedStatus(resolved, errorIDs)
+        End Function
+
+        ''' <summary>
+        ''' Resolve or re-open an array of EDT error records
+        ''' </summary>
+        ''' <param name="resolved">True to set as resolved; false to set as open</param>
+        ''' <param name="errorIDs">An array of EDT error IDs to modify</param>
+        ''' <returns>True if the action was successful; otherwise false</returns>
+        Public Function SetResolvedStatus(ByVal resolved As Boolean, ByVal errorIDs As String()) As Boolean
+            Dim spName As String = "ICIS_EDT_SetResolvedStatus"
+
+            Dim p1 As OracleParameter = New OracleParameter("resolved", resolved.ToString)
+
+            Dim p2 As New OracleParameter
+            p2.OracleDbType = OracleDbType.Varchar2
+            p2.Direction = ParameterDirection.Input
+            p2.CollectionType = OracleCollectionType.PLSQLAssociativeArray
+            p2.Value = errorIDs
+            p2.Size = errorIDs.Length
+
+            Dim parameters As OracleParameter() = {p1, p2}
+
+            Return DB.SPRunCommand(spName, parameters)
+        End Function
+
+        ''' <summary>
+        ''' Set the user responsible for a specific EDT error report
+        ''' </summary>
+        ''' <param name="userId">The user ID of the user assigned to the error</param>
+        ''' <param name="errorID">The EDT error ID to modify</param>
+        ''' <returns>True if the action was successful; otherwise false</returns>
+        Public Function AssignErrorToUser(ByVal userId As Integer, ByVal errorID As String) As Boolean
+            Dim errorIDs As String() = {errorID}
+            Return AssignErrorToUser(userId, errorIDs)
+        End Function
+
+        ''' <summary>
+        ''' Set the user responsible for an array of EDT error reports
+        ''' </summary>
+        ''' <param name="userId">The user ID of the user assigned to the errors</param>
+        ''' <param name="errorIDs">An array of EDT error IDs to modify</param>
+        ''' <returns>True if the action was successful; otherwise false</returns>
+        Public Function AssignErrorToUser(ByVal userId As Integer, ByVal errorIDs As String()) As Boolean
+            Dim spName As String = "ICIS_EDT_ReassignError"
+
+            Dim p1 As OracleParameter = New OracleParameter("userID", userId)
+
+            Dim p2 As New OracleParameter
+            p2.OracleDbType = OracleDbType.Varchar2
+            p2.Direction = ParameterDirection.Input
+            p2.CollectionType = OracleCollectionType.PLSQLAssociativeArray
+            p2.Value = errorIDs
+            p2.Size = errorIDs.Length
+
+            Dim parameters As OracleParameter() = {p1, p2}
+
+            Return DB.SPRunCommand(spName, parameters)
+        End Function
+
+#End Region
+
+    End Module
+
+End Namespace
--- IAIP/_Data Classes/DMU.EdtError.vb
+++ IAIP/_Data Classes/DMU.EdtError.vb
@@ -1,25 +1,228 @@
-﻿Namespace IAIP
-    Public Class EdtError
+﻿Namespace DMU
 
-        Public Property ID() As String
-            Get
-                Return _id
-            End Get
-            Set(ByVal value As String)
-                _id = value
-            End Set
-        End Property
-        Private _id As String
+    ''' <summary>
+    ''' An EDT Error Message category and description
+    ''' </summary>
+    Public Class EdtErrorMessage
 
         Public Property ErrorCode() As String
             Get
-                Return _errorCode
+                Return m_ErrorCode
             End Get
             Set(ByVal value As String)
-                _errorCode = value
+                m_ErrorCode = value
             End Set
         End Property
-        Private _errorCode As String
+        Private m_ErrorCode As String
+        Public Property ErrorMessage() As String
+            Get
+                Return m_ErrorMessage
+            End Get
+            Set(ByVal value As String)
+                m_ErrorMessage = value
+            End Set
+        End Property
+        Private m_ErrorMessage As String
+        Public Property ErrorCategory() As String
+            Get
+                Return m_ErrorCategory
+            End Get
+            Set(ByVal value As String)
+                m_ErrorCategory = value
+            End Set
+        End Property
+        Private m_ErrorCategory As String
+        Public Property BusinessRuleCode() As String
+            Get
+                Return m_BusinessRuleCode
+            End Get
+            Set(ByVal value As String)
+                m_BusinessRuleCode = value
+            End Set
+        End Property
+        Private m_BusinessRuleCode As String
+        Public Property BusinessRuleMessage() As String
+            Get
+                Return m_BusinessRuleMessage
+            End Get
+            Set(ByVal value As String)
+                m_BusinessRuleMessage = value
+            End Set
+        End Property
+        Private m_BusinessRuleMessage As String
+
+        Public Property DefaultUserID() As Integer
+            Get
+                Return m_DefaultUserID
+            End Get
+            Set(ByVal value As Integer)
+                m_DefaultUserID = value
+            End Set
+        End Property
+        Private m_DefaultUserID As Integer
+        Public Property DefaultUserName() As String
+            Get
+                Return m_DefaultUserName
+            End Get
+            Set(ByVal value As String)
+                m_DefaultUserName = value
+            End Set
+        End Property
+        Private m_DefaultUserName As String
 
     End Class
+
+    ''' <summary>
+    ''' An individual record submitted to the ICIS-Air EDT process
+    ''' </summary>
+    Public Class EdtSubmission
+
+        Public Property EdtTableName() As String
+            Get
+                Return m_EdtTableName
+            End Get
+            Set(ByVal value As String)
+                m_EdtTableName = value
+            End Set
+        End Property
+        Private m_EdtTableName As String
+        Public Property EdtID() As String
+            Get
+                Return m_EdtID
+            End Get
+            Set(ByVal value As String)
+                m_EdtID = value
+            End Set
+        End Property
+        Private m_EdtID As String
+        Public Property EdtForeignKeyID() As String
+            Get
+                Return m_EdtForeignKeyID
+            End Get
+            Set(ByVal value As String)
+                m_EdtForeignKeyID = value
+            End Set
+        End Property
+        Private m_EdtForeignKeyID As String
+        Public Property EdtOperation() As String
+            Get
+                Return m_EdtOperation
+            End Get
+            Set(ByVal value As String)
+                m_EdtOperation = value
+            End Set
+        End Property
+        Private m_EdtOperation As String
+        Public Property EdtStatus() As String
+            Get
+                Return m_EdtStatus
+            End Get
+            Set(ByVal value As String)
+                m_EdtStatus = value
+            End Set
+        End Property
+        Private m_EdtStatus As String
+        Public Property EdtSubmitDate() As DateTime
+            Get
+                Return m_EdtSubmitDate
+            End Get
+            Set(ByVal value As DateTime)
+                m_EdtSubmitDate = value
+            End Set
+        End Property
+        Private m_EdtSubmitDate As DateTime
+
+    End Class
+
+    ''' <summary>
+    ''' An individual error record recorded by the ICIS-Air EDT submission process
+    ''' </summary>
+    Public Class EdtError
+
+        Public Property ErrorID() As Integer
+            Get
+                Return m_ErrorID
+            End Get
+            Set(ByVal value As Integer)
+                m_ErrorID = value
+            End Set
+        End Property
+        Private m_ErrorID As Integer
+        Public Property ErrorMessage() As EdtErrorMessage
+            Get
+                Return m_ErrorMessage
+            End Get
+            Set(ByVal value As EdtErrorMessage)
+                m_ErrorMessage = value
+            End Set
+        End Property
+        Private m_ErrorMessage As EdtErrorMessage
+        Public Property EdtErrorMessageDetail() As String
+            Get
+                Return m_EdtErrorMessageDetail
+            End Get
+            Set(ByVal value As String)
+                m_EdtErrorMessageDetail = value
+            End Set
+        End Property
+        Private m_EdtErrorMessageDetail As String
+        Public Property EdtSubmission() As EdtSubmission
+            Get
+                Return m_EdtSubmission
+            End Get
+            Set(ByVal value As EdtSubmission)
+                m_EdtSubmission = value
+            End Set
+        End Property
+        Private m_EdtSubmission As EdtSubmission
+
+        Public Property AssignedToUserID() As Integer
+            Get
+                Return m_AssignedToUserID
+            End Get
+            Set(ByVal value As Integer)
+                m_AssignedToUserID = value
+            End Set
+        End Property
+        Private m_AssignedToUserID As Integer
+
+        Public Property Resolved() As Boolean
+            Get
+                Return m_Resolved
+            End Get
+            Set(ByVal value As Boolean)
+                m_Resolved = value
+            End Set
+        End Property
+        Private m_Resolved As Boolean
+        Public Property ResolvedDate() As DateTime
+            Get
+                Return m_ResolvedDate
+            End Get
+            Set(ByVal value As DateTime)
+                m_ResolvedDate = value
+            End Set
+        End Property
+        Private m_ResolvedDate As DateTime
+        Public Property ResolvedByUserID() As Integer
+            Get
+                Return m_ResolvedByUserID
+            End Get
+            Set(ByVal value As Integer)
+                m_ResolvedByUserID = value
+            End Set
+        End Property
+        Private m_ResolvedByUserID As Integer
+        Public Property ResolvedByUserName() As String
+            Get
+                Return m_ResolvedByUserName
+            End Get
+            Set(ByVal value As String)
+                m_ResolvedByUserName = value
+            End Set
+        End Property
+        Private m_ResolvedByUserName As String
+
+    End Class
+
 End Namespace
