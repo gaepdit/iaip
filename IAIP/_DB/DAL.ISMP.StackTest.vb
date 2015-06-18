603ef5e31e2b27b1bb55263f26af783e607f9353
Doug Waldron <douglas.waldron@dnr.ga.gov>
2015-06-18 16:09:40 -0400

    Re-enable ability to clear all data from stack test (fix #342) (ref #260)

--- IAIP/_DB/DAL.ISMP.StackTest.vb
+++ IAIP/_DB/DAL.ISMP.StackTest.vb
@@ -1,10 +1,16 @@
 ﻿Imports Oracle.DataAccess.Client
 Imports Iaip.Apb.SSCP
+Imports System.Collections.Generic
 
 Namespace DAL.ISMP
 
     Module StackTest
 
+        ''' <summary>
+        ''' Determines if a stack test reference number exists in the database.
+        ''' </summary>
+        ''' <param name="referenceNumber">The stack test reference number to check</param>
+        ''' <returns>True if the reference number exists; otherwise, false.</returns>
         Public Function StackTestExists(ByVal referenceNumber As String) As Boolean
             If referenceNumber = "" OrElse Not Integer.TryParse(referenceNumber, Nothing) Then Return False
 
@@ -17,18 +23,28 @@ Namespace DAL.ISMP
             Return DB.GetBoolean(query, parameter)
         End Function
 
-        Public Function TestNotificationExists(ByVal referenceNumber As String) As Boolean
-            If referenceNumber = "" OrElse Not Integer.TryParse(referenceNumber, Nothing) Then Return False
+        ''' <summary>
+        ''' Determines if a stack test notification exists in the database.
+        ''' </summary>
+        ''' <param name="notificationNumber">The stack test notification number to check</param>
+        ''' <returns>True if the notification exists; otherwise, false.</returns>
+        Public Function TestNotificationExists(ByVal notificationNumber As String) As Boolean
+            If notificationNumber = "" OrElse Not Integer.TryParse(notificationNumber, Nothing) Then Return False
 
             Dim query As String = "SELECT '" & Boolean.TrueString & "' " & _
                 " FROM AIRBRANCH.ISMPTESTNOTIFICATION " & _
                 " WHERE RowNum = 1 " & _
-                " AND STRTESTLOGNUMBER = :ReferenceNumber "
-            Dim parameter As New OracleParameter("ReferenceNumber", referenceNumber)
+                " AND STRTESTLOGNUMBER = :NotificationNumber "
+            Dim parameter As New OracleParameter("NotificationNumber", notificationNumber)
 
             Return DB.GetBoolean(query, parameter)
         End Function
 
+        ''' <summary>
+        ''' Determines if a stack test has been closed out.
+        ''' </summary>
+        ''' <param name="referenceNumber">The stack test reference number to check</param>
+        ''' <returns>True if the stack test has been closed out; otherwise, false.</returns>
         Public Function StackTestIsClosedOut(ByVal referenceNumber As String) As Boolean
             If referenceNumber = "" OrElse Not Integer.TryParse(referenceNumber, Nothing) Then Return False
 
@@ -40,11 +56,11 @@ Namespace DAL.ISMP
             Return DB.GetBoolean(query, parameter)
         End Function
 
-        Public Function GetStackTestDocumentType(ByVal referenceNumber As String) As String
+        Private Function GetStackTestDbTable(ByVal referenceNumber As String) As String
             If referenceNumber = "" OrElse Not Integer.TryParse(referenceNumber, Nothing) Then Return Nothing
 
             Dim query As String = _
-            "SELECT dt.STRDOCUMENTTYPE " & _
+            "SELECT dt.STRTABLENAME " & _
             "FROM AIRBRANCH.ISMPDocumentType dt " & _
             "INNER JOIN AIRBRANCH.ISMPReportInformation ri " & _
             "ON ri.STRDOCUMENTTYPE = dt.STRKEY " & _
@@ -53,6 +69,39 @@ Namespace DAL.ISMP
             Dim parameter As New OracleParameter("ReferenceNumber", referenceNumber)
 
             Return DB.GetSingleValue(Of String)(query, parameter)
+        End Function
+
+        ''' <summary>
+        ''' Clears all data from a stack test and resets the document type (but does not delete the test).
+        ''' </summary>
+        ''' <param name="referenceNumber">The reference number of the stack test to clear.</param>
+        ''' <returns>True if the action was successful; otherwise, false.</returns>
+        Public Function ClearStackTestData(ByVal referenceNumber As String) As Boolean
+            If referenceNumber = "" OrElse Not Integer.TryParse(referenceNumber, Nothing) Then Return Nothing
+
+            Dim tableName As String = GetStackTestDbTable(referenceNumber)
+
+            If tableName Is Nothing Then
+                Return Nothing
+            End If
+
+            Dim queryList As New List(Of String)
+            Dim parametersList As New List(Of OracleParameter())
+            Dim parameter As OracleParameter() = New OracleParameter() { _
+                New OracleParameter("ReferenceNumber", referenceNumber) _
+            }
+
+            If tableName <> "UNASSIGNED" Then
+                queryList.Add("DELETE FROM AIRBRANCH." & tableName & " WHERE strReferenceNumber = :ReferenceNumber")
+                parametersList.Add(parameter)
+            End If
+
+            queryList.Add(" UPDATE AIRBRANCH.ISMPReportInformation " & _
+                          " SET strDocumentType      = '001' " & _
+                          " WHERE strReferenceNumber = :ReferenceNumber ")
+            parametersList.Add(parameter)
+
+            Return DB.RunCommand(queryList, parametersList)
         End Function
 
     End Module
