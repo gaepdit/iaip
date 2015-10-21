dec1d47cfa8c450611a27d2268492fe68897f6ce
Doug Waldron <douglas.waldron@dnr.ga.gov>
2015-10-21 10:55:30 -0400

    Update Oracle.ManagedDataAccess NuGet package and refresh app.config

--- IAIP/My Project/Settings.Designer.vb
+++ IAIP/My Project/Settings.Designer.vb
@@ -78,14 +78,14 @@ Namespace My
             End Set
         End Property
         
-        <Global.System.Configuration.UserScopedSettingAttribute(), _
-         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(), _
-         Global.System.Configuration.DefaultSettingValueAttribute("True")> _
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("True")>  _
         Public Property JustInstalled() As Boolean
             Get
-                Return CType(Me("JustInstalled"), Boolean)
+                Return CType(Me("JustInstalled"),Boolean)
             End Get
-            Set(value As Boolean)
+            Set
                 Me("JustInstalled") = value
             End Set
         End Property
