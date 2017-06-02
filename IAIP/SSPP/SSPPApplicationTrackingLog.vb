gaepdit/iaip

commit fb33996e87cfc468337eae296e3a912a85839a36

Insert null instead of empty string where appropriate
IAIP-524

IAIP/SSPP/SSPPApplicationTrackingLog.vb
@@ -5344,9 +5344,7 @@ Public Class SSPPApplicationTrackingLog
                     }
                     Dim recordExists As Boolean = DB.ValueExists(query, parameter)
 
-                    InformationRequested = Replace(Mid(txtInformationRequested.Text, 1, 4000), "'", "''")
-
-                    InformationReceived = Mid(Replace(txtInformationReceived.Text, "'", "''"), 1, 4000)
+                    InformationReceived = Mid(txtInformationReceived.Text, 1, 4000)
 
                     If DTPInformationRequested.Checked = True Then
                         DateInfoRequested = DTPInformationRequested.Text

IAIP/DMU/DMUEisGecoTool.vb
@@ -4574,8 +4574,11 @@ Public Class DMUEisGecoTool
                 End If
 
                 ' Update EIS_QAAdmin with new facilities
-                Dim query3 As String = "INSERT INTO EIS_QAAdmin (INVENTORYYEAR, FACILITYSITEID, DATDATEQASTART, DATDATEQAPASS, QASTATUSCODE, DATQASTATUS, STRDMURESPONSIBLESTAFF, DATQACOMPLETE, STRCOMMENT, ACTIVE, UPDATEUSER, UPDATEDATETIME, CREATEDATETIME, STRFITRACKINGNUMBER, STRPOINTTRACKINGNUMBER, STRFIERROR, STRPOINTERROR)
-                    SELECT @INVENTORYYEAR, @FACILITYSITEID, GETDATE(), '', '1', GETDATE(), @UPDATEUSER, '', '', '1', @UPDATEUSER, GETDATE(), GETDATE(), '', '', '', ''
+                Dim query3 As String = "INSERT INTO EIS_QAAdmin 
+                    (INVENTORYYEAR, FACILITYSITEID, DATDATEQASTART, QASTATUSCODE, DATQASTATUS, STRDMURESPONSIBLESTAFF, ACTIVE, 
+                    UPDATEUSER, UPDATEDATETIME, CREATEDATETIME)
+                    SELECT @INVENTORYYEAR, @FACILITYSITEID, GETDATE(), '1', GETDATE(), @UPDATEUSER, '1', 
+                    @UPDATEUSER, GETDATE(), GETDATE()
                     WHERE NOT EXISTS (SELECT * FROM EIS_QAAdmin
                     WHERE inventoryYear = @INVENTORYYEAR AND FacilitySiteID = @FACILITYSITEID) 
                     AND EXISTS (SELECT * FROM EIS_Admin
@@ -6187,10 +6190,10 @@ Public Class DMUEisGecoTool
                   "EISAccessCode = '2', " &
                   "EISStatusCode = '3', " &
                   "datEISstatus = GETDATE(), " &
-                  "UpdateUser = '" & Replace(CurrentUser.AlphaName, "'", "''") & "', " &
+                  "UpdateUser = @UpdateUser, " &
                   "updatedatetime = getdate() " &
-                  "where inventoryYear = '" & EISConfirm & "' " &
-                  "and facilitysiteid = '" & txtEILogSelectedAIRSNumber.Text & "' "
+                  "where inventoryYear = @inventoryYear " &
+                  "and facilitysiteid = @facilitysiteid "
                 Dim params2 As SqlParameter() = {
                     New SqlParameter("@UpdateUser", CurrentUser.AlphaName),
                     New SqlParameter("@inventoryYear", EISConfirm),
@@ -6283,7 +6286,7 @@ Public Class DMUEisGecoTool
         Try
 
             Dim SQL As String = "Update EIS_Admin set " &
-          "strMailout = '' " &
+          "strMailout = null " &
           "where inventoryYear = @inventoryYear " &
           "and strMailout = '1' " &
           "and Active = '1' "

IAIP/Facility/Sub Forms/IAIPFacilityCreator.vb
@@ -454,22 +454,22 @@ Public Class IAIPFacilityCreator
                 PlantDesc = "N/A"
             End If
             If txtContactSocialTitle.Text <> "" Then
-                ContactPrefix = Replace(txtContactSocialTitle.Text, "'", "''")
+                ContactPrefix = txtContactSocialTitle.Text
             Else
                 ContactPrefix = ""
             End If
             If txtContactPedigree.Text <> "" Then
-                ContactSuffix = Replace(txtContactPedigree.Text, "'", "''")
+                ContactSuffix = txtContactPedigree.Text
             Else
                 ContactSuffix = ""
             End If
             If txtContactFirstName.Text <> "" Then
-                ContactFirstName = Replace(txtContactFirstName.Text, "'", "''")
+                ContactFirstName = txtContactFirstName.Text
             Else
                 ContactFirstName = "N/A"
             End If
             If txtContactLastName.Text <> "" Then
-                ContactLastName = Replace(txtContactLastName.Text, "'", "''")
+                ContactLastName = txtContactLastName.Text
             Else
                 ContactLastName = "N/A"
             End If
@@ -610,17 +610,16 @@ Public Class IAIPFacilityCreator
             DB.RunCommand(SQL, p4)
 
             SQL = "INSERT INTO APBSUPPLAMENTALDATA " &
-                "( STRAIRSNUMBER, DATSSCPTESTREPORTDUE, STRMODIFINGPERSON, DATMODIFINGDATE " &
-                ", STRDISTRICTOFFICE, STRCMSMEMBER, STRAFSACTIONNUMBER, STRRMPID) " &
+                "( STRAIRSNUMBER, STRMODIFINGPERSON, DATMODIFINGDATE " &
+                ", STRDISTRICTOFFICE, STRAFSACTIONNUMBER, STRRMPID) " &
                 "VALUES " &
-                "( @STRAIRSNUMBER, '', @STRMODIFINGPERSON, getdate() " &
+                "( @STRAIRSNUMBER, @STRMODIFINGPERSON, getdate() " &
                 ", @STRDISTRICTOFFICE, @STRCMSMEMBER, @STRAFSACTIONNUMBER, @STRRMPID) "
 
             Dim p5 As SqlParameter() = {
                 New SqlParameter("@STRAIRSNUMBER", AIRSNumber),
                 New SqlParameter("@STRMODIFINGPERSON", CurrentUser.UserID),
                 New SqlParameter("@STRDISTRICTOFFICE", DistrictOffice),
-                New SqlParameter("@STRCMSMEMBER", Nothing),
                 New SqlParameter("@STRAFSACTIONNUMBER", "00001"),
                 New SqlParameter("@STRRMPID", RMPNumber)
             }

IAIP/Fees/PASPFeeAuditLog.vb
@@ -4757,11 +4757,11 @@ Public Class PASPFeeAuditLog
             End If
 
             SQL = "Insert into FS_Admin " &
-                "(NUMFEEYEAR, STRAIRSNUMBER, STRENROLLED, DATINITIALENROLLMENT, DATENROLLMENT, STRINITIALMAILOUT, " &
+                "(NUMFEEYEAR, STRAIRSNUMBER, STRENROLLED, DATENROLLMENT, STRINITIALMAILOUT, " &
                 "STRMAILOUTSENT, DATMAILOUTSENT, INTSUBMITTAL, DATSUBMITTAL, NUMCURRENTSTATUS, DATSTATUSDATE, STRCOMMENT, " &
                 "ACTIVE, UPDATEUSER, UPDATEDATETIME, CREATEDATETIME) " &
                 "values " &
-                "(@NUMFEEYEAR, @STRAIRSNUMBER, @STRENROLLED, '', @DATENROLLMENT, @STRINITIALMAILOUT, " &
+                "(@NUMFEEYEAR, @STRAIRSNUMBER, @STRENROLLED, @DATENROLLMENT, @STRINITIALMAILOUT, " &
                 "@STRMAILOUTSENT, @DATMAILOUTSENT, @INTSUBMITTAL, @DATSUBMITTAL, 1, getdate(), @STRCOMMENT, " &
                 "1, @UPDATEUSER, getdate(), getdate() ) "
 
@@ -4816,7 +4816,7 @@ Public Class PASPFeeAuditLog
 
             If Enrolled = False Then
                 SQL = SQL & "strEnrolled = '0', " &
-                "datEnrollment = '', "
+                "datEnrollment = null, "
             Else
                 SQL = SQL & "strEnrolled = '1', "
                 SQL = SQL & "datEnrollment = @datEnrollment, "
@@ -4836,7 +4836,7 @@ Public Class PASPFeeAuditLog
 
             If MailoutSent = False Then
                 SQL = SQL & "strMailOutsent = '0', " &
-                "datMailOutSent = '', "
+                "datMailOutSent = null, "
             Else
                 SQL = SQL & "strMailOutSent = '1', "
                 SQL = SQL & "datMailOutSent = @datMailOutSent, "
@@ -4844,7 +4844,7 @@ Public Class PASPFeeAuditLog
 
             If Submittal = False Then
                 SQL = SQL & "intSubmittal = '0', " &
-                "datSubmittal = '', "
+                "datSubmittal = null, "
             Else
                 SQL = SQL & "intsubmittal = '1', "
                 SQL = SQL & "datSubmittal = @datSubmittal, "

IAIP/Fees/PASPFeeManagement.vb
@@ -893,8 +893,8 @@ Public Class PASPFeeManagement
 
             SQL = "Update FS_Admin set " &
             "strEnrolled = '0', " &
-            "datEnrollment = '', " &
-            "datInitialEnrollment = '', " &
+            "datEnrollment = null, " &
+            "datInitialEnrollment = null, " &
             "updateUser = @user, " &
             "UpdateDateTime = getdate() " &
             "where numFeeYear = @year " &

IAIP/Fees/PASPFeeStatistics.vb
@@ -1864,15 +1864,15 @@ Public Class PASPFeeStatistics
                                 "INTVOCTONS, INTPMTONS, INTSO2TONS, INTNOXTONS, " &
                                 "NUMPART70FEE, NUMSMFEE, NUMNSPSFEE, NUMTOTALFEE, " &
                                 "STRNSPSEXEMPT, STRNSPSREASON, STROPERATE, NUMFEERATE, " &
-                                "STRNSPSEXEMPTREASON, STRPART70, STRSYNTHETICMINOR, NUMCALCULATEDFEE, " &
-                                "STRCLASS1, STRNSPS1, SHUTDATE, VARIANCECHECK, VARIANCECOMMENTS, NUMADMINFEE ) " &
+                                "STRPART70, STRSYNTHETICMINOR, NUMCALCULATEDFEE, " &
+                                "NUMADMINFEE ) " &
                             "values " &
                             "(@airs, @year, " &
                             "'0', '0', '0', '0', " &
                             "'0', '0', '0', '0', " &
                             "'NO', '0', 'YES', '33.0', " &
-                            "'', 'No', 'No', '0', " &
-                            "'', '', '', '', '', '0') "
+                            "'No', 'No', '0', " &
+                            "'0') "
 
                             DB.RunCommand(SQL, p)
                         End If

IAIP/ISMP/ISMPTestReportAdministrative.vb
@@ -1974,28 +1974,20 @@ Public Class ISMPTestReportAdministrative
                    "strTestingFirm, strReviewingEngineer, strWitnessingEngineer, " &
                    "strWitnessingEngineer2, strReviewingUnit, datReviewedbyUnitManager, " &
                    "strComplianceManager, datTestDateStart, datTestDateEnd, " &
-                   "datReceivedDate, datCompleteDate, mmoCommentArea, strClosed, " &
-                   "strDirector, strCommissioner, strProgramManager, " &
-                   "strComplianceStatus, strcc, strModifingPerson, datModifingDate, " &
-                   "strControlEquipmentData, strDelete, numReviewingManager) " &
-                "values " &
-                "(@strReferenceNumber, '00001', " &
-                "'N/A', '001', " &
-                "'001', 'N/A', " &
-                "'00001', '329', " &
-                "'0', '0', " &
-                "'12', @DateReceived, " &
-                "'0', '04-Jul-1776', " &
-                "'04-Jul-1776', @DateReceived , " &
-                "@DateCompleted, " &
-                "'Historical Test report added to IAIP', " &
-                "'False', @Commissioner, " &
-                "@Director, @ProgramManager, " &
-                "'01', '0', " &
-                "@user,  GETDATE() , " &
-                "'N/A', '', " &
-                "'', '', " &
-                "'', '', '') "
+                   "datReceivedDate, datCompleteDate, mmoCommentArea, " &
+                   "strClosed, strDirector, strCommissioner, " &
+                   "strProgramManager, strComplianceStatus, strcc, " &
+                   "strModifingPerson, datModifingDate, strControlEquipmentData) " &
+               "values " &
+                "(@strReferenceNumber, '00001', 'N/A', " &
+                "'001', '001', 'N/A', " &
+                "'00001', '329', '0', " &
+                "'0', '12', @DateReceived, " &
+                "'0', '04-Jul-1776', '04-Jul-1776', " &
+                "@DateReceived , @DateCompleted, 'Historical Test report added to IAIP', " &
+                "'False', @Director, @Commissioner, " &
+                "@ProgramManager, '01', '0', " &
+                "@user,  GETDATE() , 'N/A') "
 
                 Dim p4 As SqlParameter() = {
                     New SqlParameter("@strReferenceNumber", RefNum),

IAIP/SBEAP/SBEAPClientSummary.vb
@@ -1391,7 +1391,6 @@ Public Class SBEAPClientSummary
                         , DATCOMPANYCREATED
                         , STRMODIFINGPERSON
                         , DATMODIFINGDATE
-                        , STRMODIFINGCOMMENTS
                         )
                         VALUES
                         ( @CLIENTID
@@ -1414,7 +1413,6 @@ Public Class SBEAPClientSummary
                         , getdate()
                         , @STRMODIFINGPERSON
                         , getdate()
-                        , ''
                         )"
 
                     Dim p As SqlParameter() = {
@@ -1457,7 +1455,6 @@ Public Class SBEAPClientSummary
                         , STRSTATEPROGRAMCODES
                         , STRMODIFINGPERSON
                         , DATMODIFINGDATE
-                        , STRMODIFINGCOMMENTS
                         , STRAIRPERMITNUMBER
                         , STRSSCPENGINEER
                         , STRSSCPUNIT
@@ -1479,7 +1476,6 @@ Public Class SBEAPClientSummary
                         , @STRSTATEPROGRAMCODES
                         , @STRMODIFINGPERSON
                         , getdate()
-                        , ''
                         , @STRAIRPERMITNUMBER
                         , @STRSSCPENGINEER
                         , @STRSSCPUNIT

commit c8df78cd2e4ea2d74adc50f214e281403b2e3f81

Fix SSPP App Tracking Log not saving Info Requested
Bug introduced in fb33996e

IAIP-599

IAIP/SSPP/SSPPApplicationTrackingLog.vb
@@ -5344,6 +5344,8 @@ Public Class SSPPApplicationTrackingLog
                     }
                     Dim recordExists As Boolean = DB.ValueExists(query, parameter)
 
+                    InformationRequested = Mid(txtInformationRequested.Text, 1, 4000)
+
                     InformationReceived = Mid(txtInformationReceived.Text, 1, 4000)
 
                     If DTPInformationRequested.Checked = True Then