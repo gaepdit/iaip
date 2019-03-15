2cf8c99b91cc4573abf8b2d488830be3e2a048d9
Doug Waldron <douglas.waldron@dnr.ga.gov>
2019-03-14 21:37:47 -0400

    Bump version 5.11.3

--- IAIP/AssemblyInfo.vb
+++ IAIP/AssemblyInfo.vb
@@ -1,7 +1,7 @@
 Imports System.Reflection
 Imports System.Runtime.InteropServices
 
-<Assembly: AssemblyVersion("5.11.2.0")>
+<Assembly: AssemblyVersion("5.11.3.0")>
 <Assembly: AssemblyCompany("State of Georgia")>
 <Assembly: AssemblyCopyright("")>
 <Assembly: AssemblyTrademark("")>
