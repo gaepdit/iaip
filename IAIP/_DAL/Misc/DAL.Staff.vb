114411a8959975800cfde37954737a76dd1175cb
Doug Waldron <douglas.waldron@dnr.ga.gov>
2019-04-15 09:48:44 -0400

    Revert "Fix bug in Event Registration tool"
    (Broke binding on staff phone number)
    
    This reverts commit db51e90ccb35dc4904051396baa5d99487525da8.
    Unfixes IAIP-960, IAIP-961
    Fixes IAIP-962

--- IAIP/_DAL/Misc/DAL.Staff.vb
+++ IAIP/_DAL/Misc/DAL.Staff.vb
@@ -3,6 +3,25 @@
 Namespace DAL
     Module StaffData
 
+        Public Function GetStaffDetailsAsDataTableByBranch(Optional branch As Integer = 1, Optional includeInactive As Boolean = False) As DataTable
+            ' Default to Air Branch if no branch code is provided
+            Dim query As String =
+                " SELECT NUMUSERID, STRLASTNAME, STRFIRSTNAME, STREMAILADDRESS, STRPHONE, NUMEMPLOYEESTATUS, " &
+                "     CONCAT(STRLASTNAME, ', ', STRFIRSTNAME) AS AlphaName " &
+                " FROM EPDUSERPROFILES " &
+                " WHERE NUMBRANCH = @branch "
+
+            If Not includeInactive Then
+                query &= " AND NUMEMPLOYEESTATUS = 1 "
+            End If
+
+            query &= " ORDER BY STRLASTNAME, STRFIRSTNAME "
+
+            Dim parameter As New SqlParameter("@branch", branch)
+
+            Return DB.GetDataTable(query, parameter)
+        End Function
+
         Public Function GetStaffAsDataTableByBranch(Optional branch As Integer = 1, Optional includeInactive As Boolean = False) As DataTable
             ' Default to Air Branch if no branch code is provided
             Dim query As String =
