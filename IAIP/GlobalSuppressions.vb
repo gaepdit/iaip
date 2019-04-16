b2986a69f5dd4a8dc3e5043d94bda17461fb7181
Doug Waldron <douglas.waldron@dnr.ga.gov>
2019-04-16 10:15:27 -0400

    CA1801: Review unused parameters
    IAIP-957

--- IAIP/GlobalSuppressions.vb
+++ IAIP/GlobalSuppressions.vb
@@ -5,3 +5,5 @@
 ' a specific target and scoped to a namespace, type, member, etc.
 
 <Assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification:="IAIP was designed around this pattern.")>
+<Assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA1801:Review unused parameters", Justification:="Parameter used in Release, but not Debug build", Scope:="member", Target:="~M:Iaip.ErrorReporting.ErrorReport(System.Exception,System.String,System.String,System.Boolean)")>
+<Assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA1801:Review unused parameters", Justification:="Parameter used in Release, but not Debug build", Scope:="member", Target:="~M:Iaip.IaipExceptionManager.HandleException(System.Exception,System.String)")>
