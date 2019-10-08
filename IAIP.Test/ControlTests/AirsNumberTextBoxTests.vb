f9acf5d8c797e0a52e2c11a7dde5589c1998ad88
Doug Waldron <douglas.waldron@dnr.ga.gov>
2019-10-08 12:14:54 -0400

    CA2000: Dispose disposable objects (unit test project)

--- IAIP.Test/ControlTests/AirsNumberTextBoxTests.vb
+++ IAIP.Test/ControlTests/AirsNumberTextBoxTests.vb
@@ -6,13 +6,13 @@ Public Class AirsNumberTextBoxTests
 
     <Fact>
     Private Sub InitialValues()
-        Dim airs As New AirsNumberTextBox()
-
-        Assert.Null(airs.AirsNumber)
-        Assert.Equal(9, airs.MaxLength)
-        Assert.Equal("000-00000", airs.Cue)
-        Assert.False(airs.FacilityMustExist)
-        Assert.Equal(AirsNumberValidationResult.Empty, airs.ValidationStatus)
+        Using airs As New AirsNumberTextBox()
+            Assert.Null(airs.AirsNumber)
+            Assert.Equal(9, airs.MaxLength)
+            Assert.Equal("000-00000", airs.Cue)
+            Assert.False(airs.FacilityMustExist)
+            Assert.Equal(AirsNumberValidationResult.Empty, airs.ValidationStatus)
+        End Using
     End Sub
 
     <Theory>
@@ -21,15 +21,13 @@ Public Class AirsNumberTextBoxTests
     <InlineData("0010001")>
     <InlineData("001-0001")>
     Private Sub InvalidFormat(input As String)
-        Dim airs As New AirsNumberTextBox With {
-            .Text = input
-        }
+        Using airs As New AirsNumberTextBox With {.Text = input}
+            Dim result As Boolean = airs.Validate()
 
-        Dim result As Boolean = airs.Validate()
-
-        Assert.True(result)
-        Assert.Equal(AirsNumberValidationResult.InvalidFormat, airs.ValidationStatus)
-        Assert.Equal(input, airs.Text)
+            Assert.True(result)
+            Assert.Equal(AirsNumberValidationResult.InvalidFormat, airs.ValidationStatus)
+            Assert.Equal(input, airs.Text)
+        End Using
     End Sub
 
     <Theory>
@@ -38,45 +36,41 @@ Public Class AirsNumberTextBoxTests
     <InlineData("99999999")>
     <InlineData("999-99999")>
     Private Sub ValidFormat(input As String)
-        Dim airs As New AirsNumberTextBox With {
-            .Text = input
-        }
+        Using airs As New AirsNumberTextBox With {.Text = input}
+            Dim result As Boolean = airs.Validate()
 
-        Dim result As Boolean = airs.Validate()
-
-        Assert.True(result)
-        Assert.Equal(AirsNumberValidationResult.Valid, airs.ValidationStatus)
-        Assert.Equal(String.Concat(input.Substring(0, 3), "-", input.Substring(input.Length - 5)), airs.Text)
+            Assert.True(result)
+            Assert.Equal(AirsNumberValidationResult.Valid, airs.ValidationStatus)
+            Assert.Equal(String.Concat(input.Substring(0, 3), "-", input.Substring(input.Length - 5)), airs.Text)
+        End Using
     End Sub
 
     <Theory>
     <InlineData(Nothing)>
     <InlineData("")>
     Private Sub EmptyInput(input As String)
-        Dim airs As New AirsNumberTextBox With {
-            .Text = input
-        }
+        Using airs As New AirsNumberTextBox With {.Text = input}
+            Dim result As Boolean = airs.Validate()
 
-        Dim result As Boolean = airs.Validate()
-
-        Assert.True(result)
-        Assert.Equal(AirsNumberValidationResult.Empty, airs.ValidationStatus)
-        Assert.Equal(String.Empty, airs.Text)
+            Assert.True(result)
+            Assert.Equal(AirsNumberValidationResult.Empty, airs.ValidationStatus)
+            Assert.Equal(String.Empty, airs.Text)
+        End Using
     End Sub
 
     <Fact>
     Private Sub ValidationStatusChanged()
-        Dim airs As New AirsNumberTextBox()
+        Using airs As New AirsNumberTextBox()
+            AddHandler airs.ValidationStatusChanged, AddressOf Airs_ValidationStatusChanged
 
-        AddHandler airs.ValidationStatusChanged, AddressOf Airs_ValidationStatusChanged
+            Assert.Null(airs.Tag)
 
-        Assert.Null(airs.Tag)
+            airs.Text = "001-00001"
+            airs.Validate()
 
-        airs.Text = "001-00001"
-        airs.Validate()
-
-        Assert.NotNull(airs.Tag)
-        Assert.True(CBool(airs.Tag))
+            Assert.NotNull(airs.Tag)
+            Assert.True(CBool(airs.Tag))
+        End Using
     End Sub
 
     Private Sub Airs_ValidationStatusChanged(sender As Object, e As EventArgs)
