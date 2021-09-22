f446817154fcc909292f25987d3f28f8576fe444
Doug Waldron <douglas.waldron@dnr.ga.gov>
2021-09-22 15:19:45 -0400

    Use Integer keyword instead of Int32

--- IAIP/_Code/BitFields.vb
+++ IAIP/_Code/BitFields.vb
@@ -12,7 +12,7 @@
         Return ConvertIntegerToEnum(Of TEnum)(ConvertBitFieldToInteger(bitField, reversed))
     End Function
 
-    Private Function ConvertBitFieldToInteger(bitField As String, Optional reversed As Boolean = True) As Int32
+    Private Function ConvertBitFieldToInteger(bitField As String, Optional reversed As Boolean = True) As Integer
         ' bitFlags must contain only zeros and ones (and not be empty)
         If bitField Is Nothing OrElse Not System.Text.RegularExpressions.Regex.IsMatch(bitField, "^[0-1]+$") Then
             Return 0
@@ -24,7 +24,7 @@
         Return Convert.ToInt32(bitField, 2)
     End Function
 
-    Private Function ConvertIntegerToEnum(Of TEnum)(i As Int32) As TEnum
+    Private Function ConvertIntegerToEnum(Of TEnum)(i As Integer) As TEnum
         'If [Enum].IsDefined(GetType(TEnum), i) Then
         Return DirectCast([Enum].ToObject(GetType(TEnum), i), TEnum)
         'Else
@@ -46,7 +46,7 @@
         Return ConvertIntegerToBitFlags(Convert.ToInt32(enumeration), totalWidth, reverse)
     End Function
 
-    Private Function ConvertIntegerToBitFlags(i As Int32, totalWidth As Integer, Optional reverse As Boolean = True) As String
+    Private Function ConvertIntegerToBitFlags(i As Integer, totalWidth As Integer, Optional reverse As Boolean = True) As String
         Dim bfs As String = Convert.ToString(i, 2).PadLeft(totalWidth, "0"c)
         If reverse Then bfs = StrReverse(bfs)
         Return bfs
