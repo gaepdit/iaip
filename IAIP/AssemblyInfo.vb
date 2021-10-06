2bf3372ee6825b611a4fc0891d72cb68559b1b2f
Doug Waldron <douglas.waldron@dnr.ga.gov>
2021-10-06 10:53:12 -0400

    Bump version: 5.26.0

--- IAIP/AssemblyInfo.vb
+++ IAIP/AssemblyInfo.vb
@@ -2,27 +2,25 @@ Imports System.Resources
 Imports System.Reflection
 Imports System.Runtime.InteropServices
 
-<Assembly: AssemblyVersion("5.25.6.0")>
+<Assembly: AssemblyVersion("5.26.0.0")>
 <Assembly: AssemblyCompany("State of Georgia")>
-<Assembly: AssemblyCopyright("")>
-<Assembly: AssemblyTrademark("")>
+<Assembly: AssemblyCopyright("Copyright © 2021")>
 <Assembly: CLSCompliant(True)>
 <Assembly: ComVisible(False)>
+<Assembly: NeutralResourcesLanguage("en-US")>
 
 #If UAT Then
 
 <Assembly: AssemblyTitle("IAIP Horizon")>
-<Assembly: AssemblyDescription("GA Air Protection Branch integrated database system; user acceptance testing version")>
+<Assembly: AssemblyDescription("GA Air Protection Branch integrated information system; user acceptance testing version")>
 <Assembly: AssemblyProduct("IAIP Horizon")> 
 <Assembly: Guid("4d6ff346-89e3-4388-9b75-0077e47a7e46")>
 
 #Else
 
 <Assembly: AssemblyTitle("Integrated Air Information Platform")>
-<Assembly: AssemblyDescription("GA Air Protection Branch integrated database system")>
+<Assembly: AssemblyDescription("GA Air Protection Branch integrated information system")>
 <Assembly: AssemblyProduct("Integrated Air Information Platform")>
 <Assembly: Guid("9F83B6AF-EE06-4324-83E9-7AB1D5E8BE5F")>
-<Assembly: NeutralResourcesLanguage("en-US")>
-
 
 #End If
