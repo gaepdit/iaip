337af3bcbed37f539c895b197ef71652a29657a1
Doug Waldron <douglas.waldron@dnr.ga.gov>
2023-01-18 11:36:53 -0500

    Bump version: 6.6.1.2

--- IAIP/AssemblyInfo.vb
+++ IAIP/AssemblyInfo.vb
@@ -2,7 +2,7 @@ Imports System.Resources
 Imports System.Reflection
 Imports System.Runtime.InteropServices
 
-<Assembly: AssemblyVersion("6.6.1.0")>
+<Assembly: AssemblyVersion("6.6.1.2")>
 <Assembly: AssemblyCompany("State of Georgia")>
 <Assembly: AssemblyCopyright("Copyright © 2023")>
 <Assembly: CLSCompliant(True)>
