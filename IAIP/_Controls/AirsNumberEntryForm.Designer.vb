496e84f36f60cbd8103d8ca6ff7c0719d5419095
Doug Waldron <douglas.waldron@dnr.ga.gov>
2025-12-16 12:09:48 -0500

    Enable ctrl-backspace in AIRS forms and nav screen

--- IAIP/_Controls/AirsNumberEntryForm.Designer.vb
+++ IAIP/_Controls/AirsNumberEntryForm.Designer.vb
@@ -19,18 +19,20 @@ Partial Class AirsNumberEntryForm
         Me.AirsEntryTextBox.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
             Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.AirsEntryTextBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest
+        Me.AirsEntryTextBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource
         Me.AirsEntryTextBox.Location = New System.Drawing.Point(0, 0)
         Me.AirsEntryTextBox.MaxLength = 9
         Me.AirsEntryTextBox.Name = "AirsEntryTextBox"
         Me.AirsEntryTextBox.Size = New System.Drawing.Size(107, 20)
         Me.AirsEntryTextBox.TabIndex = 0
         '
-        'AirNumberEntryForm
+        'AirsNumberEntryForm
         '
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
         Me.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange
         Me.Controls.Add(Me.AirsEntryTextBox)
-        Me.Name = "AirNumberEntryForm"
+        Me.Name = "AirsNumberEntryForm"
         Me.Size = New System.Drawing.Size(107, 20)
         Me.ResumeLayout(False)
         Me.PerformLayout()
